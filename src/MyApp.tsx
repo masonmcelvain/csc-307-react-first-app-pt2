41f5c1a3c83b79f23f59da1cdecd569ce5f4232e
Mason McElvain <masonmcelvain@users.noreply.github.com>
2021-04-07 08:47:49 -0700

    Fetch character data on mount

--- src/MyApp.tsx
+++ src/MyApp.tsx
@@ -1,10 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Form from './Form';
 import Table from './Table';
 import { Character } from './types';
 
+const baseEndpoint = "http://127.0.0.1:5000/";
+
 export default function MyApp() {
   const [characters, setCharacters] = useState<Character[]>([]);
+
+  // Fetch the data from the backend on mount
+  useEffect(() => {
+    axios.get(baseEndpoint + "users")
+      .then(res => {
+        setCharacters(res.data.users_list);
+      })
+      .catch(function (error) {
+        console.log("GET request for /users failed:" + error);
+      });
+  }, []);
 
   function removeOneCharacter(index: number): void {
     const updatedCharacters = characters.filter((character, i) => i !== index);
